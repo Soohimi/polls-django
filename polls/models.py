71cec380170ac901c9e933bc5d635a64084e2eab
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-20 12:04:43 +0330

    implementing custom was_publish_recently function for Question model

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +10,9 @@ class Question(models.Model):
 
     def __str__(self):
         return 'Question==' + self.question_text + ' | Date==' + self.publish_date
+    
+    def was_published_recently(self):
+        return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
