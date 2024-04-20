a757e42931a86d3152bec666383a8adb02635caa
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-20 10:55:41 +0330

    unused import removal

--- first/urls.py
+++ first/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path,include
 from django.contrib import admin
 
-from . import views
-
 urlpatterns = [
     path("polls/",include("polls.urls")),
     path("admin/",admin.site.urls),
