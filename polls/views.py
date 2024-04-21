42a75dce8add5251e40225863d9ec62f8d3f10a9
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-21 14:14:01 +0330

    implementing index view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(result):
-    return HttpResponse("polls index!")
+    question_list = Question.objects.order_by("-pub_date")[:5]
+    response = ", ".join([q.question_text for q in question_list])
+    return HttpResponse(response)
 
 def detail(req, question_id):
     return HttpResponse("Question number %s." %question_id)
