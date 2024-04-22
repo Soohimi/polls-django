d45d9203bfc4a4ffbd44eb9f0b686341dc200543
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-22 16:47:48 +0330

    replacing old views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,27 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name="latest_question_list"
 
-def index(req):
-    question_list = Question.objects.order_by("-publish_date")[:5]
-    return render(req, "polls/index.html", {"question_list": question_list})
+    def get_queryset(self):
+        return Question.objects.order_by("-publish_date")[:5]
 
-def detail(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, "polls/results.html", {"question":question})
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"        
 
 def vote(req, question_id):
     question = get_object_or_404(Question,pk=question_id)
