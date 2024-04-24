9b8580044e9876a7d6e91a05e841064dad13adf4
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-24 15:58:04 +0330

    fixing a view bug

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name="question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-publish_date")[:5]
+        return Question.objects.filter(publish_date__lte=timezone.now()).order_by("-publish_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
