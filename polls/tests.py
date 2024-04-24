686aa0140b6954b91219509e0b3d41f75dddebb7
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-24 16:03:04 +0330

    writing a passing view test

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
 
 import datetime
 
@@ -22,3 +23,18 @@ class QuestionModelTests(TestCase):
         time=timezone.now() - datetime.timedelta(hours=23,minutes=59,seconds=59,milliseconds=999)
         recent_question = Question(publish_date=time)
         self.assertIs(recent_question.was_published_recently,True)
+
+def create_question(question_text, days):
+    #Creates a question 
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    #Test case for scenarios where no questions exist and an appropriate message must be shown.
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"No polls are available.")
+        self.assertQuerySetEqual(response.context["question_list"],[])
+
+        
